cd44d19eb7363d019d9f900dfb8049d171e0112d
Xan007 <sierrasander29@gmail.com>
2025-09-23 02:23:50 -0500

    Add frontend to cors

--- Backend/src/main/java/com/hydro/watertap/config/CorsGlobalConfig.java
+++ Backend/src/main/java/com/hydro/watertap/config/CorsGlobalConfig.java
@@ -14,7 +14,7 @@ public class CorsGlobalConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://watertapclone-j2hc75u1a-sierrasander29-5645s-projects.vercel.app", "http://localhost:8080", "http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://watertapclone-j2hc75u1a-sierrasander29-5645s-projects.vercel.app", "http://localhost:8080", "http://localhost:3000", "https://watertapclone.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
